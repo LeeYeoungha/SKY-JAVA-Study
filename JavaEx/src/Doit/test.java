6f459e1e86276fb9749611d0a91f3eab0e2a39b0
LeeYeoungha <skylove2458@gmail.com>
2022-07-20 11:14:38 +0900

    DoIt exp

--- JavaEx/src/Doit/test.java
+++ JavaEx/src/Doit/test.java
@@ -1,10 +1,32 @@
 package Doit;
+/*
+ * public class test {
+ * 
+ * public static void main(String[] args) { int year = 2018;
+ * 
+ * }
+ * 
+ * }
+ */
+
+
+
+
+
+/*
+ * public class test{ public static void main(String[] args) { int age = 10;
+ * System.out.println(age); } }
+ */
 
 public class test {
-
-	public static void main(String[] args) {
-		int year = 2018;
-		
+public static void main(String[] args) {
+	
+	int i = 0;
+	
+	for(i = 0 ; i <= 10 ; i++) {
+		System.out.println("¾È³çÇÏ¼¼¿ä" + i);
 	}
 	
+	
+}
 }
