e1f6e2290c8563f652baa9d49f3622ebabcbbd72
LeeYeoungha <skylove2458@gmail.com>
2022-07-21 10:56:56 +0900

    Autech test

--- JavaEx/src/Doit/Student.java
+++ JavaEx/src/Doit/Student.java
@@ -1,4 +1,4 @@
-package Doit;
+/*package Doit;
 
 public class Student {
 	int studentID;
@@ -18,3 +18,31 @@ public class Student {
 	
 }
 
+*/
+
+package Doit;
+
+public class Student{
+	public String studentName;
+	public int grade;
+	public int money;
+	
+	public Student(String studentName, int money) {
+		this.studentName = studentName;
+		this.money = money;
+	}
+	
+	public void takeBus(Bus bus) {
+		bus.take(1000);
+		this.money -= 1000;
+	}
+	
+	public void takeSubway(Subway subway) {
+		subway.take(1500);
+		this.money -= 1500;
+	}
+	
+	public void showInfo() {
+		System.out.println(studentName + " " + money);
+	}
+}
---
+++ JavaEx/src/test/StudentTest.java
@@ -0,0 +1,25 @@
+package test;
+
+public class StudentTest {
+	
+	public static void main(String[] args) {
+		Student studentLee = new Student();
+		studentLee.setStudentName("ÀÌ¿µÇÏ");
+		
+		System.out.println(studentLee.getStudentName());
+	}
+
+}
+
+class Student{
+	int Student;
+	int setStudentName = Student;
+	public void setStudentName(String Student) {
+		// TODO Auto-generated method stub
+		
+	}
+	public char[] getStudentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
