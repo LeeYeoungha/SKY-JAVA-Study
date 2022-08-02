000c39a54cabc7b1ad4cec6a536ce3f9956cd7d3
LeeYeoungha <skylove2458@gmail.com>
2022-08-02 13:12:52 +0900

    interface test

--- JavaEx/src/test/test.java
+++ JavaEx/src/test/test.java
@@ -3,54 +3,53 @@ package test;
 //import java.lang.reflect.Constructor;
 
 //class test {
-	
-	/*
-	 * public static void main(String[] args) { System.out.println(10 + 10);
-	 * System.out.println(20 + 20); }
-	 */
-	
-	
-	/*
-	 * public static void sum(int left, int right) { System.out.println(left +
-	 * right); }
-	 * 
-	 * 
-	 * public static void main(String args[]) { sum(10,10); }
-	 */
-	
-	/*
-	 * public static void sum(int left, int right) { System.out.println(left +
-	 * right); }
-	 * 
-	 * 
-	 * public static void avg(int left, int right) { System.out.println((left +
-	 * right) / 2); }
-	 * 
-	 * public static void main(String args[]) {
-	 * 
-	 * int right , left;
-	 * 
-	 * left = 20; right = 20; sum(left, right); avg(left, right);
-	 * 
-	 * 
-	 * }
-	 */
-	/*
-	 * int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * public void sum() { System.out.println(this.left + this.right); }
-	 * 
-	 * public void avg() { System.out.println((this.left + this.right) / 2); }
-	 * 
-	 * 
-	 * public static void main(String args[]) {
-	 * 
-	 * test t1 = new test(); t1.setOperand(10,20); t1.sum(); t1.avg(); }
-	 */
-	/* } */
+
+/*
+ * public static void main(String[] args) { System.out.println(10 + 10);
+ * System.out.println(20 + 20); }
+ */
+
+/*
+ * public static void sum(int left, int right) { System.out.println(left +
+ * right); }
+ * 
+ * 
+ * public static void main(String args[]) { sum(10,10); }
+ */
+
+/*
+ * public static void sum(int left, int right) { System.out.println(left +
+ * right); }
+ * 
+ * 
+ * public static void avg(int left, int right) { System.out.println((left +
+ * right) / 2); }
+ * 
+ * public static void main(String args[]) {
+ * 
+ * int right , left;
+ * 
+ * left = 20; right = 20; sum(left, right); avg(left, right);
+ * 
+ * 
+ * }
+ */
+/*
+ * int left, right;
+ * 
+ * public void setOperand(int left, int right) { this.left = left; this.right =
+ * right; }
+ * 
+ * public void sum() { System.out.println(this.left + this.right); }
+ * 
+ * public void avg() { System.out.println((this.left + this.right) / 2); }
+ * 
+ * 
+ * public static void main(String args[]) {
+ * 
+ * test t1 = new test(); t1.setOperand(10,20); t1.sum(); t1.avg(); }
+ */
+/* } */
 /*
 int left, right;
 
@@ -129,7 +128,7 @@ public static void main(String args[]) {
  * 
  * }
  */
-	
+
 /*
  * static double pi = 3.14; static int base = 0; int left, right;
  * 
@@ -152,204 +151,357 @@ public static void main(String args[]) {
  * 
  * 
  */
-	/*
+/*
+
+public static void sum(int left, int right) {
+	System.out.println(left + right);
+}
+
+public static void avg(int left, int right) {
+	System.out.println((left + right) / 2);
+}
+
+
+public class text1 {
+
+		public static void main(String[] args) {
+		test.sum(10,10);			
+		test.avg(10,10);
+	}
+}
+
+}*/
+
+/*
+ * class C1 { static int static_variable = 1; int instance_variable = 2; static
+ * void static_static() { System.out.println(static_variable); }
+ * 
+ * static void static_instance() { // 클래스 메소드에서는 인스턴스 변수에 접근 할 수 없다.
+ * //System.out.println(instance_variable); }
+ * 
+ * 
+ * void instance_static() { // 인스턴스 메소드에서는 클래스 변수에 접근 할 수 있다.
+ * System.out.println(static_variable); }
+ * 
+ * 
+ * void instance_instance() { System.out.println(instance_variable); } }
+ * 
+ * 
+ * public class test { public static void main(String[] args) { C1 c = new C1();
+ * // 인스턴스를 이용해서 정적 메소드에 접근 -> 성공 // 인스턴스 메소드가 정적 변수에 접근 -> 성공
+ * c.static_static(); // 인스턴스를 이용해서 정적 메소드에 접근 -> 성공 // 정적 메소드가 인스턴스 변수에 접근 ->
+ * 실패 c.static_instance(); // 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공 // 인스턴스 메소드가 클래스 변수에
+ * 접근 -> 성공 c.instance_static(); // 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공 // 인스턴스 메소드가
+ * 인스턴스 변수에 접근 -> 성공 c.instance_instance(); // 클래스를 이용해서 클래스 메소드에 접근 -> 성공 //
+ * 클래스 메소드가 클래스 변수에 접근 -> 성공 C1.static_static(); // 클래스를 이용해서 클래스 메소드에 접근 -> 성공
+ * // 클래스 메소드가 인스턴스 변수에 접근 -> 실패 C1.static_instance(); // 클래스를 이용해서 인스턴스 메소드에 접근
+ * -> 실패 //C1.instance_static(); // 클래스를 이용해서 인스턴스 메소드에 접근 -> 실패
+ * //C1.instance_instance(); } }
+ */
+
+/*
+ * public class test { static int i;
+ * 
+ * static void a() { i = 0; }
+ * 
+ * public static void main(String[] args) {
+ * 
+ * for(int i=0; i<=10; i++) { a(); System.out.println(i); } } }
+ */
+
+/*
+ * class tete {
+ * 
+ * int left, right;
+ * 
+ * public void setOperand(int left, int right) { this.left = left; this.right =
+ * right; }
+ * 
+ * 
+ * public void sum() { System.out.println(left + right); }
+ * 
+ * public void avg() { System.out.println((left + right) / 2); }
+ * 
+ * }
+ * 
+ * 
+ * 
+ * public class test { public static void main(String[] args) {
+ * 
+ * tete t1 = new tete(); t1.setOperand(10, 10); t1.sum(); t1.avg();
+ * 
+ * } }
+ */
+
+/*
+ * 
+ * 
+ * class Calculator {
+ * 
+ * int left, right;
+ * 
+ * public void setOperand(int left, int right) { this.left = left; this.right =
+ * right; }
+ * 
+ * public void sum() { System.out.println(left + right); }
+ * 
+ * public void avg() { System.out.println((left + right) / 2); } }
+ * 
+ * class SubstractionableCalculator extends Calculator { public void substract()
+ * { System.out.println(this.left - this.right); } }
+ * 
+ * 
+ * public class test { public static void main(String[] args) {
+ * SubstractionableCalculator c1 = new SubstractionableCalculator();
+ * c1.setOperand(10,20); c1.sum(); c1.avg(); c1.substract(); } }
+ * 
+ */
+
+/*
+ * class calculator {
+ * 
+ * int left, right;
+ * 
+ * public void setOperand(int left, int right) { this.left = left; this.right =
+ * right; }
+ * 
+ * public void sum() { System.out.println(left + right); }
+ * 
+ * }
+ * 
+ * 
+ * class extendTest extends calculator { public void min() {
+ * System.out.println(left - right); } }
+ * 
+ * 
+ * public class test{ public static void main(String[] args) { extendTest t1 =
+ * new extendTest(); t1.setOperand(10,10); t1.sum(); t1.min(); } }
+ */
+
+/*
+ * class calculator { int left, right;
+ * 
+ * public void setOperand(int left, int right) { this.left = left; this.right =
+ * right; }
+ * 
+ * public void sum() { System.out.println(left + right); } }
+ * 
+ * 
+ * class calculatorSumExtend extends calculator { public void min() {
+ * System.out.println(left - right); }
+ * 
+ * }
+ * 
+ * public class test{
+ * 
+ * public static void main(String[] args) { calculatorSumMinAvg cs1 = new
+ * calculatorSumMinAvg(); cs1.setOperand(10,20); cs1.sum(); cs1.min();
+ * cs1.avg(); cs1.test(); }
+ * 
+ * }
+ * 
+ * class calculatorSumMin extends calculatorSumExtend { public void avg() {
+ * System.out.println((left+right)/2); } }
+ * 
+ * 
+ * class calculatorSumMinAvg extends calculatorSumMin { public void test() {
+ * System.out.println("left"); } }
+ */
+
+/*
+ * public class test {
+ * 
+ * public test() {} public test(int param1) {}
+ * 
+ * 
+ * public static void main(String[] args) { test t1 = new test(); } }
+ */
+
+/*
+class calculator {
+	int left, right;
+
+	public void setOperand(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public void sum() {
+		System.out.println(left + right);
+	}
+
+	public void avg() {
+		System.out.println((left + right) / 2);
+	}
+}
+
+class SubstractioableCalculator extends calculator {
+
+	public void min() {
+		System.out.println(left - right);
+	}
+
+}
+
+public class test {
+	public static void main(String[] args) {
+		SubstractioableCalculator sc1 = new SubstractioableCalculator();
+		sc1.setOperand(10, 20);
+		sc1.sum();
+		sc1.avg();
+		sc1.min();
+	}
+}
+  */
+
+/*
+class Calculator {
 	
-	public static void sum(int left, int right) {
+	int left, right;
+	
+	public Calculator() {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public void setOperand(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public void sum() {
 		System.out.println(left + right);
 	}
 	
-	public static void avg(int left, int right) {
-		System.out.println((left + right) / 2);
+	public void avg() {
+		System.out.println((left + right)/2);
+	}
+
+	
+}
+
+class min extends Calculator {
+	public void SubCalculator(int left, int right){
+	this.left = left;
+	this.right = right;
+	}
+	
+	public void min() {
+		System.out.println(left - right);
+	}
+	
+}
+
+public class test {
+	public static void main(String[] args) {
+		Calculator C1 = new Calculator(); 
+			C1.setOperand(20,20);
+			C1.sum();
+			C1.avg();
+		
+			min m1 = new min();
+			m1.setOperand(40,30);
+			m1.min();			
+	}
+}
+*/
+
+/*
+class Calculator {
+    int left, right;
+    Calculator(){}
+     
+    public Calculator(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+     
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+ 
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+ 
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+}
+ 
+class SubstractionableCalculator extends Calculator {
+    public SubstractionableCalculator(int left, int right) {
+		this.left = left;
+		this.right = right;
+    	super(left,right);
+    }
+ 
+    public void substract() {
+        System.out.println(this.left - this.right);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        SubstractionableCalculator c1 = new SubstractionableCalculator(10, 20);
+        c1.sum();
+        c1.avg();
+        c1.substract();
+    }
+}
+
+*/
+
+
+class calculator{
+	int left, right;
+	
+	public void setOperand(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public void sum() {
+		System.out.println(left + right);
 	}
 	
 	
-	public class text1 {
+}
 
-			public static void main(String[] args) {
-			test.sum(10,10);			
-			test.avg(10,10);
-		}
+class extExtends extends calculator{
+	public void min() {
+		System.out.println(left - right);
+	}
+}
+
+class interTest implements interfaceTest{
+
+	@Override
+	public int sum() {
+		System.out.println("interface 테스트 sum()");
+		return 0;
+	}
+
+	@Override
+	public int min() {
+		System.out.println("interface 테스트 min()");
+		return 0;
 	}
 	
-}*/
-	 
-	/*
-	 * class C1 { static int static_variable = 1; int instance_variable = 2; static
-	 * void static_static() { System.out.println(static_variable); }
-	 * 
-	 * static void static_instance() { // 클래스 메소드에서는 인스턴스 변수에 접근 할 수 없다.
-	 * //System.out.println(instance_variable); }
-	 * 
-	 * 
-	 * void instance_static() { // 인스턴스 메소드에서는 클래스 변수에 접근 할 수 있다.
-	 * System.out.println(static_variable); }
-	 * 
-	 * 
-	 * void instance_instance() { System.out.println(instance_variable); } }
-	 * 
-	 * 
-	 * public class test { public static void main(String[] args) { C1 c = new C1();
-	 * // 인스턴스를 이용해서 정적 메소드에 접근 -> 성공 // 인스턴스 메소드가 정적 변수에 접근 -> 성공
-	 * c.static_static(); // 인스턴스를 이용해서 정적 메소드에 접근 -> 성공 // 정적 메소드가 인스턴스 변수에 접근 ->
-	 * 실패 c.static_instance(); // 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공 // 인스턴스 메소드가 클래스 변수에
-	 * 접근 -> 성공 c.instance_static(); // 인스턴스를 이용해서 인스턴스 메소드에 접근 -> 성공 // 인스턴스 메소드가
-	 * 인스턴스 변수에 접근 -> 성공 c.instance_instance(); // 클래스를 이용해서 클래스 메소드에 접근 -> 성공 //
-	 * 클래스 메소드가 클래스 변수에 접근 -> 성공 C1.static_static(); // 클래스를 이용해서 클래스 메소드에 접근 -> 성공
-	 * // 클래스 메소드가 인스턴스 변수에 접근 -> 실패 C1.static_instance(); // 클래스를 이용해서 인스턴스 메소드에 접근
-	 * -> 실패 //C1.instance_static(); // 클래스를 이용해서 인스턴스 메소드에 접근 -> 실패
-	 * //C1.instance_instance(); } }
-	 */
-	 
-	 
+}
 
-	/*
-	 * public class test { static int i;
-	 * 
-	 * static void a() { i = 0; }
-	 * 
-	 * public static void main(String[] args) {
-	 * 
-	 * for(int i=0; i<=10; i++) { a(); System.out.println(i); } } }
-	 */
-
-
-	/*
-	 * class tete {
-	 * 
-	 * int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * 
-	 * public void sum() { System.out.println(left + right); }
-	 * 
-	 * public void avg() { System.out.println((left + right) / 2); }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * public class test { public static void main(String[] args) {
-	 * 
-	 * tete t1 = new tete(); t1.setOperand(10, 10); t1.sum(); t1.avg();
-	 * 
-	 * } }
-	 */
-
-
-
-	/*
-	 * 
-	 * 
-	 * class Calculator {
-	 * 
-	 * int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * public void sum() { System.out.println(left + right); }
-	 * 
-	 * public void avg() { System.out.println((left + right) / 2); } }
-	 * 
-	 * class SubstractionableCalculator extends Calculator { public void substract()
-	 * { System.out.println(this.left - this.right); } }
-	 * 
-	 * 
-	 * public class test { public static void main(String[] args) {
-	 * SubstractionableCalculator c1 = new SubstractionableCalculator();
-	 * c1.setOperand(10,20); c1.sum(); c1.avg(); c1.substract(); } }
-	 * 
-	 */
-
-
-	/*
-	 * class calculator {
-	 * 
-	 * int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * public void sum() { System.out.println(left + right); }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * class extendTest extends calculator { public void min() {
-	 * System.out.println(left - right); } }
-	 * 
-	 * 
-	 * public class test{ public static void main(String[] args) { extendTest t1 =
-	 * new extendTest(); t1.setOperand(10,10); t1.sum(); t1.min(); } }
-	 */
-
-
-	/*
-	 * class calculator { int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * public void sum() { System.out.println(left + right); } }
-	 * 
-	 * 
-	 * class calculatorSumExtend extends calculator { public void min() {
-	 * System.out.println(left - right); }
-	 * 
-	 * }
-	 * 
-	 * public class test{
-	 * 
-	 * public static void main(String[] args) { calculatorSumMinAvg cs1 = new
-	 * calculatorSumMinAvg(); cs1.setOperand(10,20); cs1.sum(); cs1.min();
-	 * cs1.avg(); cs1.test(); }
-	 * 
-	 * }
-	 * 
-	 * class calculatorSumMin extends calculatorSumExtend { public void avg() {
-	 * System.out.println((left+right)/2); } }
-	 * 
-	 * 
-	 * class calculatorSumMinAvg extends calculatorSumMin { public void test() {
-	 * System.out.println("left"); } }
-	 */
-
-	/*
-	 * public class test {
-	 * 
-	 * public test() {} public test(int param1) {}
-	 * 
-	 * 
-	 * public static void main(String[] args) { test t1 = new test(); } }
-	 */
-
-
-
-	/*
-	 * 
-	 * class calculator{ int left, right;
-	 * 
-	 * public void setOperand(int left, int right) { this.left = left; this.right =
-	 * right; }
-	 * 
-	 * public void sum() { System.out.println(left + right); }
-	 * 
-	 * public void avg( ) { System.out.println((left + right) / 2); } }
-	 * 
-	 * 
-	 * class SubstractioableCalculator extends calculator {
-	 * 
-	 * public void min() { System.out.println(left - right); }
-	 * 
-	 * }
-	 * 
-	 * 
-	 * public class test{ public static void main(String[] args) {
-	 * SubstractioableCalculator sc1 = new SubstractioableCalculator();
-	 * sc1.setOperand(10, 20); sc1.sum(); sc1.avg(); sc1.min(); } }
-	 * 
-	 */
-
-
+public class test {
+	
+	public static void main(String[] args) {
+		
+		extExtends ex1 = new extExtends();
+		ex1.setOperand(10,10);
+		ex1.sum();
+		ex1.min();
+		
+		interTest in1 = new interTest();
+		in1.sum();
+		in1.min();
+	}
+}
