095b5bcaf923446e7476a3df5edaf4f35ceba21b
skylo <skylo@192.168.219.109>
2022-08-26 00:15:14 +0900

    te

--- JavaEx/src/knowProgram/com/Addition.java
+++ JavaEx/src/knowProgram/com/Addition.java
@@ -1,17 +1,44 @@
 package knowProgram.com;
 
+import java.util.Scanner;
+
 public class Addition {
 	
 	public static void main(String[]args) {
 		
-		int num1, num2, sum;
+		//ex1
+		/*		int num1, num2, sum;
+				
+				num1 = 10;
+				num2 = 20;
+				
+				sum = num1 + num2;
+				
+				System.out.printf("%d + %d = %d",num1,num2,sum);
+				*/
 		
-		num1 = 10;
-		num2 = 20;
 		
-		sum = num1 + num2;
+		//ex2
 		
-		System.out.printf("%d + %d = %d",num1,num2,sum);
+		System.out.println("input num");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		/*int inputSc = sc.nextInt();
+		int inputScc = sc.nextInt();*/
+		
+		double inputSc = sc.nextDouble();
+		double inputScc = sc.nextDouble();
+		
+		System.out.printf("%d, %d 을 입력%n", inputSc, inputScc);
+		
+		double sum = inputSc + inputScc;
+		
+		System.out.println("합계값" + sum + "이다");
+		
+		
+		
+		
 		
 	}
 
