0b556697d6e344890fd51b21563e1b687ce4016d
skylo <skylo@192.168.219.109>
2022-08-26 23:39:16 +0900

    add

--- JavaEx/src/knowProgram/com/arrayLoop.java
+++ JavaEx/src/knowProgram/com/arrayLoop.java
@@ -4,7 +4,7 @@ public class arrayLoop {
 
 	public static void main(String[] args) {
 
-		int arrayB[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+		int arrayB[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 		int i = 0;
 		int x = 5;
 		int C = 0;
